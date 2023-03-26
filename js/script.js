d780639c5747327cfc62fdb46f3cd60be401b51f
Daniel <110478467+DanielDiacon@users.noreply.github.com>
2023-03-26 20:27:52 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -7,6 +7,9 @@ if (burgerMenu) {
       burgerMenu.classList.toggle('_active');
       menuBody.classList.toggle('_active');
    });
+   menuBody.addEventListener('click', function (e) {
+      menuBody.classList.remove('_active');
+   });
 };
 // loading page <=>
 const loader = document.getElementById("loader");
@@ -15,21 +18,24 @@ window.addEventListener("load", function () {
    loader.style.opacity = "0";
    loader.style.transition = "0.7s";
 });
-
-
-
-const items = document.querySelectorAll('.menu__item');
-const sections = document.querySelectorAll('section');
-
-items.forEach(item => {
-   item.addEventListener('click', () => {
-      const currentActiveItem = document.querySelector('.active');
-      const currentActiveSection = document.getElementById(currentActiveItem.dataset.section);
-      const targetSection = document.getElementById(item.dataset.section);
-
-      currentActiveItem.classList.remove('active');
-      item.classList.add('active');
-      currentActiveSection.classList.add('hidden');
-      targetSection.classList.remove('hidden');
+// burger <=>
+function activateItem(item, sectionId) {
+   // Remove the active class from all items
+   const items = document.querySelectorAll(".menu__item");
+   items.forEach((item) => {
+      item.classList.remove("active");
    });
-});
+
+   // Add the active class to the clicked item
+   item.classList.add("active");
+
+   // Remove the active class from all sections
+   const sections = document.querySelectorAll(".section");
+   sections.forEach((section) => {
+      section.classList.remove("active-section");
+   });
+
+   // Add the active class to the corresponding section
+   const section = document.getElementById(sectionId);
+   section.classList.add("active-section");
+}
